334520edad321929fc06e0e7061d9f154e9177dc
Peter Njiru <peternjirum@gmail.com>
2023-07-22 13:50:55 -0400

    completes instance methods lab

--- lib/dog.py
+++ lib/dog.py
@@ -3,5 +3,24 @@
 class Dog:
     # Class body goes here
 
-    #Instance method definition
-    pass
+    # Instance method definition
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")    
+
+# Note the use of the self keyword in our instance method definition above. 
+# We will explore the concept of self in depth in the next module- just know 
+# for now that it allows methods to access the other methods and attributes. 
+# self is required in instance method definitions, though you don't actually 
+# need to pass it in when you call the method.
+
+fido = Dog()
+fido.bark()
+
+snoopy = Dog()
+snoopy.bark()
+
+# instance methods, the methods that belong to particular instances of particular 
+# classes, cannot be used globally like procedural methods. They cannot be called without an instance.
